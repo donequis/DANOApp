bbe2ba547a1def9cfd2fdc24a71cb63bcd1d4638
DamianMarquez <66253810+DamianMarquez@users.noreply.github.com>
2021-02-06 11:17:08 -0300

    Add files via upload

---
+++ src/main/java/DANO/DANOApp/DanoAppApplication.java
@@ -0,0 +1,13 @@
+package DANO.DANOApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DANO/DANOApp/DanoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package DANO.DANOApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
