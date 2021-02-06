c24b42ed8528e886ecc4603464a77454bf3d1eb6
Damian Marquez <dmarquez@baufest.com>
2021-02-06 12:00:57 -0300

    Agregado Prueba

--- src/main/java/DANO/DANOApp/DanoAppApplication.java
+++ src/main/java/DANO/DANOApp/DanoAppApplication.java
@@ -1,7 +1,12 @@
 package DANO.DANOApp;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DanoAppApplication {
@@ -9,5 +14,21 @@ public class DanoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DanoAppApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
 
 }
---
+++ src/main/java/DANO/DANOApp/Web/homaController.java
@@ -0,0 +1,15 @@
+package DANO.DANOApp.Web;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class homaController {
+
+	@RequestMapping("/home")
+	public String index() {
+		return "<html><body>    <iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/OckMmA0JQIY\" frameborder=\"0\"        allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\"        allowfullscreen></iframe>    <div id=\"yappa-comments-frame\" data-disable-ads=\"false\"></div>    <script type=\"text/javascript\" src=\"https://comments.yappaapp.com/embed/yappa-comments.js\"></script><a href=\"pagina2.html\">Prueba2</a></body></html>";
+	}
+
+}
